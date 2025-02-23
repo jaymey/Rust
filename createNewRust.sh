37cd783d92b8b13285d7c7ccdfa70f30c4c52986
jaymey <jmeyer1124@gmail.com>
2025-02-23 18:54:15 -0500

    change

--- createNewRust.sh
+++ createNewRust.sh
@@ -3,19 +3,4 @@
 cargo new $1
 cd $1
 
-cargo add tokio
-cargo add color-eyre
-cargo add tracing
-cargo add reqwest
-cargo add rayon
-cargo add aws-sdk-rust
-cargo add clap
-cargo add sqlx
-cargo add chrono
-cargo add egui
-cargo add yew
-cargo clippy --fix -- \
--W clippy::pedantic \
--W clippy::nursery \
--W clippy::unwrap_used \
--W clippy::expect_used
+cp ../addCrates.sh .
